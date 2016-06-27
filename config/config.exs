e306bcd275ed61931d20257e46f2d2c6c157b3f6
tim <llStrainll@gmail.com>
2016-06-27 22:13:54 +0300

    upd wwwest_lite

--- config/config.exs
+++ config/config.exs
@@ -47,5 +47,5 @@ config :wwwest_lite,
 	server_timeout: 120000, # timeout for all requests
 	memo_ttl: 5000, # timeout for memorize json encode and decode
 	callback_module: Studio.WwwestLite, # here are handlers for requests
-	post_data_type: :any, # | :xml | :any # it's data type of post q for decoding
+	post_data_type: :protobuf, # | :xml | :any # it's data type of post q for decoding
 	crossdomain: true
