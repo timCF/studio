de03ecfce1578449f99705f660b6295a2d477adf
tim <tim@dis.lan>
2016-08-09 17:10:55 +0300

    add sessions sort, upd admin ui

--- lib/loaders/superadmin.ex
+++ lib/loaders/superadmin.ex
@@ -5,7 +5,9 @@ defmodule Studio.Loaders.Superadmin do
 	]
 	use Cachex, [ttl: 1000, export: true, serialize_on_init: false]
 	defp read_callback(_) do
-		data = Studio.Storage.fullstate
+		data = %Studio.Proto.FullState{sessions: sessions, sessions_template: sessions_template} = Studio.Storage.fullstate
+		data = %Studio.Proto.FullState{data | sessions: Enum.sort(sessions, fn(%Studio.Proto.Session{time_from: ts1}, %Studio.Proto.Session{time_from: ts2}) -> ts1 < ts2 end),
+							sessions_template: Enum.sort(sessions_template, fn(%Studio.Proto.SessionTemplate{min_from: ts1}, %Studio.Proto.SessionTemplate{min_from: ts2}) -> ts1 < ts2 end)}
 		hash = Studio.hash(data)
 		%{data: %Studio.Proto.Response{status: :RS_ok_state, message: "", state: %Studio.Proto.FullState{data | hash: hash}}, hash: hash}
 	end
