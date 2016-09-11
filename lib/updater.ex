1e459232725f89a8315953c721fb689160267766
tim <tim@MacBook-Pro-tim.local>
2016-09-11 16:57:35 +0300

    optimize updater

--- lib/updater.ex
+++ lib/updater.ex
@@ -7,21 +7,32 @@ defmodule Studio.Updater do
 	definit do
 		{:ok, nil, @ttl}
 	end
-	definfo :timeout do
-		_ = Studio.Loaders.Superadmin.get(:data) |> auto_derive_prices
-		{:noreply, nil, @ttl}
+	definfo :timeout, state: state do
+		newstate = Studio.Loaders.Superadmin.get(:data)
+		_ = auto_derive_prices(state, newstate)
+		{:noreply, newstate, @ttl}
 	end
 
-	defp auto_derive_prices(nil), do: :ok
-	defp auto_derive_prices(%Studio.Proto.Response{state: state = %Studio.Proto.FullState{sessions: sessions}}) do
-		Logger.info("auto upd start")
+	defp auto_derive_prices(state, state), do: :ok
+	defp auto_derive_prices(nil, %Studio.Proto.Response{state: state = %Studio.Proto.FullState{sessions: sessions}}) do
+		Logger.info("auto upd start (INIT)")
 		:rpc.pmap({__MODULE__, :auto_derive_prices_process}, [state], sessions)
+		|> Enum.each(fn
+			:ok -> :ok
+			sess = %Studio.Proto.Session{} -> _ = Studio.Worker.do_work(fn() -> Studio.Storage.maybe_update_session_amount(sess) end)
+		end)
+		Logger.info("auto upd end (INIT)")
+	end
+	defp auto_derive_prices(%Studio.Proto.Response{state: %Studio.Proto.FullState{sessions: old_sessions}}, %Studio.Proto.Response{state: state = %Studio.Proto.FullState{sessions: sessions}}) do
+		Logger.info("auto upd start")
+		:rpc.pmap({__MODULE__, :auto_derive_prices_process}, [state], cut_off_extra_sessions(old_sessions, sessions))
 		|> Enum.each(fn
 			:ok -> :ok
 			sess = %Studio.Proto.Session{} -> _ = Studio.Worker.do_work(fn() -> Studio.Storage.maybe_update_session_amount(sess) end)
 		end)
 		Logger.info("auto upd end")
 	end
+	defp auto_derive_prices(_, _), do: :ok
 
 	def auto_derive_prices_process(sess = %Studio.Proto.Session{status: status}, state = %Studio.Proto.FullState{}) when (status in [:SS_awaiting_last, :SS_awaiting_first]) do
 		derived_amount = Studio.Utils.derive_session_price(sess, state)
@@ -31,5 +42,17 @@ defmodule Studio.Updater do
 		end
 	end
 	def auto_derive_prices_process(%Studio.Proto.Session{}, %Studio.Proto.FullState{}), do: :ok
+
+	defp cut_off_extra_sessions(old_sess, new_sess) do
+		[old_sess_map, new_sess_map] = Enum.map([old_sess, new_sess], fn(lst) -> Enum.group_by(lst, fn(%Studio.Proto.Session{band_id: id}) -> id end)  end)
+		Stream.filter(new_sess_map, fn({id, lst}) ->
+			case Map.get(old_sess_map, id) do
+				^lst -> false
+				_ -> true
+			end
+		end)
+		|> Stream.flat_map(fn({_, lst}) -> lst end)
+		|> Enum.filter(fn(%Studio.Proto.Session{status: status}) -> (status in [:SS_awaiting_last, :SS_awaiting_first]) end)
+	end
 
 end
