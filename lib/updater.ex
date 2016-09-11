b57328bc5f8fdcccbd451008d4e4f92813710e88
tim <tim@MacBook-Pro-tim.local>
2016-09-11 15:26:26 +0300

    auto upd in pmap

--- lib/updater.ex
+++ lib/updater.ex
@@ -14,14 +14,22 @@ defmodule Studio.Updater do
 
 	defp auto_derive_prices(nil), do: :ok
 	defp auto_derive_prices(%Studio.Proto.Response{state: state = %Studio.Proto.FullState{sessions: sessions}}) do
-		Enum.each(sessions, fn
-			sess = %Studio.Proto.Session{status: status} when (status in [:SS_awaiting_last, :SS_awaiting_first]) ->
-				derived_amount = Studio.Utils.derive_session_price(sess, state)
-				new_sess = %Studio.Proto.Session{sess | amount: derived_amount, price: derived_amount}
-				_ = Studio.Worker.do_work(fn() -> Studio.Storage.maybe_update_session_amount(new_sess) end)
-			%Studio.Proto.Session{} ->
-				:ok
+		Logger.info("auto upd start")
+		:rpc.pmap({__MODULE__, :auto_derive_prices_process}, [state], sessions)
+		|> Enum.each(fn
+			:ok -> :ok
+			sess = %Studio.Proto.Session{} -> _ = Studio.Worker.do_work(fn() -> Studio.Storage.maybe_update_session_amount(sess) end)
 		end)
+		Logger.info("auto upd end")
 	end
+
+	def auto_derive_prices_process(sess = %Studio.Proto.Session{status: status}, state = %Studio.Proto.FullState{}) when (status in [:SS_awaiting_last, :SS_awaiting_first]) do
+		derived_amount = Studio.Utils.derive_session_price(sess, state)
+		case %Studio.Proto.Session{sess | amount: derived_amount, price: derived_amount} do
+			^sess -> :ok
+			new_sess -> new_sess
+		end
+	end
+	def auto_derive_prices_process(%Studio.Proto.Session{}, %Studio.Proto.FullState{}), do: :ok
 
 end
