a21261bf16b9af3c27fe670bb73b2d6ad8b902b3
tim <tim@MacBook-Pro-tim.local>
2016-09-18 17:16:18 +0300

    -

--- lib/loaders/superadmin.ex
+++ lib/loaders/superadmin.ex
@@ -3,7 +3,7 @@ defmodule Studio.Loaders.Superadmin do
 		{"@refresh_message", (%Studio.Proto.Response{status: :RS_refresh, message: "", state: %Studio.Proto.FullState{hash: ""}} |> Studio.encode)},
 		{"@server_names", (Application.get_env(:pmaker, :servers) |> Enum.map(fn(%{module: module}) -> module end))}
 	]
-	use Cachex, [ttl: 1000, export: true, serialize_on_init: false]
+	use Cachex, [ttl: 200, export: true, serialize_on_init: false]
 	defp read_callback(_) do
 		data = %Studio.Proto.FullState{sessions: sessions, sessions_template: sessions_template} = Studio.Storage.fullstate
 		data = %Studio.Proto.FullState{data | sessions: Enum.sort(sessions, fn(%Studio.Proto.Session{time_from: ts1}, %Studio.Proto.Session{time_from: ts2}) -> ts1 < ts2 end),
